7c3035d33207091648d55f75a553efe4e07676b5
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-09-24 10:15:01 +0100

    ✨ Loadbalancer for public subnets

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -5,70 +5,106 @@ locals {
     private_a = "172.31.50.0/24"
     private_b = "172.31.51.0/24"
   }
-  subnets = {
-    public_a = {
-        cidr_block = local.subnet_ips.public_a
-        availability_zone = "${data.aws_region.current.name}a"
-        map_public_ip_on_launch = true
-    }
-    public_a = {
-        cidr_block = local.subnet_ips.public_b
-        availability_zone = "${data.aws_region.current.name}b"
-        map_public_ip_on_launch = true
-    }
-    private_a = {
-        cidr_block = local.subnet_ips.private_a
-        availability_zone = "${data.aws_region.current.name}a"
-        map_public_ip_on_launch = false
-    }
-    private_b = {
-        cidr_block = local.subnet_ips.private_b
-        availability_zone = "${data.aws_region.current.name}b"
-        map_public_ip_on_launch = false
-    }
-  }
 }
 
 resource "aws_default_vpc" "default" {
-
 }
 
 data "aws_region" "current" {
 }
 
-resource "aws_subnet" "subnet" {
-  for_each = local.subnets
-
-  vpc_id = aws_default_vpc.default.id
-  cidr_block = each.value.cidr_block
-  availability_zone = each.value.availability_zone
-  map_public_ip_on_launch = each.value.map_public_ip_on_launch
-
-  tags = {
-    Name = each.key
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_default_vpc.default.id
+  cidr_block              = local.subnet_ips.public_a
+  availability_zone       = "${data.aws_region.current.name}a"
+  map_public_ip_on_launch = true
+  tags                    = {
+    Name = "public_a"
   }
 }
 
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_default_vpc.default.id
+  cidr_block              = local.subnet_ips.public_b
+  availability_zone       = "${data.aws_region.current.name}b"
+  map_public_ip_on_launch = true
+  tags                    = {
+    Name = "public_b"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id                  = aws_default_vpc.default.id
+  availability_zone       = "${data.aws_region.current.name}a"
+  cidr_block              = local.subnet_ips.private_a
+  map_public_ip_on_launch = false
+  tags                    = {
+    Name = "private_a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id                  = aws_default_vpc.default.id
+  availability_zone       = "${data.aws_region.current.name}b"
+  cidr_block              = local.subnet_ips.private_b
+  map_public_ip_on_launch = false
+  tags                    = {
+    Name = "private_b"
+  }
+}
 resource "aws_route_table" "private" {
   vpc_id = aws_default_vpc.default.id
 }
 
 resource "aws_route_table_association" "public_a_to_default" {
   route_table_id = aws_default_vpc.default.default_route_table_id
-  subnet_id = aws_subnet.subnet["public_a"].id
+  subnet_id = aws_subnet.public_a.id
 }
 
 resource "aws_route_table_association" "public_b_to_default" {
   route_table_id = aws_default_vpc.default.default_route_table_id
-  subnet_id = aws_subnet.subnet["public_b"].id
+  subnet_id = aws_subnet.public_b.id
 }
-
 resource "aws_route_table_association" "private_a_to_private" {
   route_table_id = aws_route_table.private.id
-  subnet_id = aws_subnet.subnet["private_a"].id
+  subnet_id = aws_subnet.private_a.id
 }
 
 resource "aws_route_table_association" "private_b_to_private" {
   route_table_id = aws_route_table.private.id
-  subnet_id = aws_subnet.subnet["private_b"].id
+  subnet_id = aws_subnet.private_b.id
+}
+
+resource "aws_security_group" "load_balancer" {
+  name = "${var.namespace}-${var.project_name}-alb"
+  description = "Allows traffic to the load balancer"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    self = false
+  }
+  egress {
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_lb" "load_balancer" {
+  name = "${var.namespace}-${var.project_name}-alb"
+  load_balancer_type = "application"
+  internal = false
+  subnets = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+  security_groups = [
+    aws_security_group.load_balancer.id
+  ]
 }
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -4,9 +4,16 @@ output "vpc" {
 
 output "subnet" {
   value = {
-    public_a  = aws_subnet.subnet["public_a"]
-    public_b  = aws_subnet.subnet["public_b"]
-    private_a = aws_subnet.subnet["private_a"]
-    private_b = aws_subnet.subnet["private_b"]
+    public_a  = aws_subnet.public_a
+    public_b  = aws_subnet.public_b
+    private_a = aws_subnet.private_a
+    private_b = aws_subnet.private_b
+  }
+}
+
+output "alb" {
+  value = {
+    lb = aws_lb.load_balancer
+    sg = aws_security_group.load_balancer
   }
 }
