2331e0b57b84ffb38760b0cacec0617ef880bd43
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-09-24 12:50:47 +0100

    🔥

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">=1.5.7"
-  backend "s3" {
-    bucket = "state-bucket-22092048"
-    key = "terraform.tfstate"
-    region = "eu-west-2"
-    encrypt = true
-    dynamodb_table = "state-bucket-lock-22092048"
-  }
+  # backend "s3" {
+  #   bucket = "state-bucket-22092048"
+  #   key = "terraform.tfstate"
+  #   region = "eu-west-2"
+  #   encrypt = true
+  #   dynamodb_table = "state-bucket-lock-22092048"
+  # }
 
   required_providers {
     aws = {
